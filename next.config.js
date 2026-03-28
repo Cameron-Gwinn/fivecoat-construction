ba7fbd17d01c4f6e3e8a381ec5cd3f9e52eeeee0
Cameron-Gwinn <cameron@jasejones.com>
2026-03-27 22:51:54 -0500

    Switch to GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/fivecoat-construction",
   trailingSlash: true,
   images: {
     unoptimized: true,
